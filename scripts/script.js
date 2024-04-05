8845e53c222d570af0e444f222c781e9a66ff728
kris5321 <krystianwiatr321@gmail.com>
2024-04-05 15:55:35 +0200

    ciag dalszy
    dodanie animacji

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,17 @@ function wege() {
     // Przekierowanie użytkownika na inną stronę
     window.location.href = "https://www.rp.pl/zdrowy-styl-zycia/art37419791-wegetarianizm-moze-byc-szkodliwy-dla-rozwoju-mozgu-inteligencji-i-zdrowia-psychicznego";
 }
+
+// Dodaj poniższy kod na końcu skryptu.js
+
+// Po załadowaniu strony
+document.addEventListener("DOMContentLoaded", function() {
+    // Pokaż logo3 przez 3 sekundy, a następnie ukryj
+    var logo3 = document.querySelector('.logo3');
+    logo3.style.opacity = '1';
+    setTimeout(function() {
+        logo3.style.opacity = '0';
+    }, 1500);
+});
+
+
