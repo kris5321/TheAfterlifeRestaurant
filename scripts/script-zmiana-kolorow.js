10d45f5c2c7e50df9c6afa6c66e86173bcb76a78
xKerelek <karolnowak3217@gmail.com>
2024-05-01 20:45:52 +0200

    Etap 1 - dodanie nowych rzeczy

---
+++ scripts/script-zmiana-kolorow.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const napis = document.querySelector('.nazwa-restauracji');
+    napis.classList.add('animated');
+});
--- scripts/script.js
+++ scripts/script.js
@@ -2,19 +2,21 @@ function wege() {
     window.location.href = "https://www.rp.pl/zdrowy-styl-zycia/art37419791-wegetarianizm-moze-byc-szkodliwy-dla-rozwoju-mozgu-inteligencji-i-zdrowia-psychicznego";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    var logo3 = document.querySelector('.logo3');
-    logo3.style.opacity = '0'; 
-    logo3.style.transition = 'opacity 1.5s ease'; 
-    setTimeout(function() {
-        logo3.style.opacity = '1'; 
-    }, 100);
-
-
-    setTimeout(function() {
-        logo3.style.opacity = '0'; 
-    }, 2000); 
+document.addEventListener('DOMContentLoaded', function() {
+    anime({
+        targets: '.logo3',
+        opacity: [
+            {value: 0, duration: 0},
+            {value: 1, duration: 1500, easing: 'easeInOutQuad'},
+            {value: 0, duration: 1500, easing: 'easeInOutQuad', delay: 2000}
+        ],
+        complete: function(anim) {
+            // Po zakończeniu animacji usuń logo
+            document.querySelector('.logo3').remove();
+        }
+    });
 });
+
 
 
 
